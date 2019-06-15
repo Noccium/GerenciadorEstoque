b4e3a123cc992361525b7cbc89fc48dd3fa907f0
Lucas Felipe <lucasousa2009@outlook.com>
2019-06-15 19:16:44 -0300

    Update README.md

--- gerenciador-estoque/app/forms.py
+++ gerenciador-estoque/app/forms.py
@@ -9,7 +9,7 @@ class CadastroForm(FlaskForm):
     quantidadeAtual = IntegerField('Quantidade Atual')
     quantidadeMin = IntegerField('Quantidade Mínima')
     consumoDiario = IntegerField('Consumo Diário')
-    submit = SubmitField('Submeter')
+    submit = SubmitField('Cadastrar')
 
     def validate_nome(self, nome):
         nome = Mantimento.query.filter_by(nome=nome.data).first()
@@ -44,5 +44,51 @@ class CadastroForm(FlaskForm):
                 consumoDiario.errors = []
                 raise ValidationError('Quantidade apenas de 0 até 100!')
 
-class VisualizarForm(FlaskForm):
-    pass
+class AtualizarForm(FlaskForm):
+    nome = StringField('Nome', validators=[InputRequired(message="Campo Obrigatório!"),
+                                           Length(min=1, max=20, message='Máximo 20 caracteres')])
+    novoNome = StringField('Novo Nome', validators=[InputRequired(message="Campo Obrigatório!"),
+                                           Length(min=1, max=20, message='Máximo 20 caracteres')])
+    descricao = TextAreaField('Descrição', validators=[Length(min=0, max=120, message='Máximo 120 caracteres')])
+    quantidadeAtual = IntegerField('Quantidade Atual')
+    quantidadeMin = IntegerField('Quantidade Mínima')
+    consumoDiario = IntegerField('Consumo Diário')
+    submit = SubmitField('Atualizar')
+
+    def validate_nome(self, nome):
+        nome = Mantimento.query.filter_by(nome=nome.data).first()
+        if nome == None:
+            raise ValidationError('Mantimento não cadastrado.')
+
+    def validate_novoNome(self, novoNome):
+        novoNome = Mantimento.query.filter_by(nome=novoNome.data).first()
+        if novoNome:
+            if novoNome.nome != self.nome.data:
+                raise ValidationError('Nome já cadastrado.')
+
+    def validate_quantidadeAtual(self, quantidadeAtual):
+        if (quantidadeAtual.data == None):
+            quantidadeAtual.errors = []
+            raise ValidationError('Preencha com números!')
+        else:
+            if quantidadeAtual.data > 100 or quantidadeAtual.data < 0:
+                quantidadeAtual.errors = []
+                raise ValidationError('Quantidade apenas de 0 até 100!')
+
+    def validate_quantidadeMin(self, quantidadeMin):
+        if (quantidadeMin.data == None):
+            quantidadeMin.errors = []
+            raise ValidationError('Preencha com números!')
+        else:
+            if quantidadeMin.data > 100 or quantidadeMin.data < 0:
+                quantidadeMin.errors = []
+                raise ValidationError('Quantidade apenas de 0 até 100!')
+
+    def validate_consumoDiario(self, consumoDiario):
+        if (consumoDiario.data == None):
+            consumoDiario.errors = []
+            raise ValidationError('Preencha com números!')
+        else:
+            if consumoDiario.data > 100 or consumoDiario.data < 0:
+                consumoDiario.errors = []
+                raise ValidationError('Quantidade apenas de 0 até 100!')
--- gerenciador-estoque/app/routes.py
+++ gerenciador-estoque/app/routes.py
@@ -1,12 +1,32 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, db
-from app.forms import CadastroForm
+from app.forms import CadastroForm, AtualizarForm
 from app.models import Mantimento, LogQuantidade
+
+@app.route('/atualizar', methods=['GET', 'POST'])
+def atualizar():
+    form = AtualizarForm()
+    if form.validate_on_submit():
+        flash('Mantimento {} atualizado com sucesso!'.format(
+            form.nome.data),'sucess')
+        mantimento = Mantimento.query.filter_by(nome=form.nome.data).first()
+        log = LogQuantidade(quantidadeAntiga=mantimento.quantidadeAtual, quantidadeNova=form.quantidadeAtual.data,
+                            mantimento_id=mantimento.id)
+        db.session.add(log)
+        db.session.commit()
+        mantimento.nome = form.novoNome.data
+        mantimento.descricao = form.descricao.data
+        mantimento.quantidadeAtual = form.quantidadeAtual.data
+        mantimento.quantidadeMin = form.quantidadeMin.data
+        mantimento.consumoDiario = form.consumoDiario.data
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('atualizar.html', title='Atualizar', form=form, db=db)
 
 @app.route('/visualizar')
 def visualizar():
     mantimento = Mantimento.query.all()
-    return render_template('visualizar.html', title='Visualizar Mantimento', mantimento = mantimento)
+    return render_template('visualizar.html', title='Visualizar', mantimento = mantimento)
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
